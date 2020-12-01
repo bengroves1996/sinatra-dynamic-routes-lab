2ef9f85b6a64440e669fe43019644868709cce43
bengroves <bengroves15@gmail.com>
2020-12-01 15:25:52 -0500

    commit tree c0065b0d8a8ad2a66a9b98dfb0da3061ed165639

--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    eman = params[:name].reverse
+    "#{eman}"
+  end
+
+  get "/square/:number" do
+    @num = params[:number]
+    num_i =  @num.to_i
+    "#{num_i*num_i}"
+  end
+
+  get "/say/:number/:phrase" do
+    @word = params[:phrase]
+    @times = params[:number]
+    amt = @times.to_i
+    "#{@word*amt}"
+  end 
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word_one = params[:word1]
+    @word_two = params[:word2]
+    @word_three = params[:word3]
+    @word_four = params[:word4]
+    @word_five = params[:word5]
+    "#{@word_one} #{@word_two} #{@word_three} #{@word_four} #{@word_five}."
+  end 
+
+  get '/:operation/:number1/:number2' do 
+    num1 = params[:number1]
+    num2 = params[:number2]
+    op = params[:operation]
+  end 
 
 end
